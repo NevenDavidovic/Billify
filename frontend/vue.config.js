4f3fb795725372b4bb73fd12d0b10c7940443b90
NevenDavidovic <nenoronnie@gmmail.com>
2024-01-29 22:06:54 +0100

    Konfiguracija less-a za homepage i stilova

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          strictMath: true,
+          // Other Less options if needed
+        },
+      },
+      postcss: {
+        postcssOptions: {
+          plugins: [
+            require("postcss-nested")(), // Add postcss-nested plugin
+            // Other PostCSS plugins if needed
+          ],
+        },
+      },
+    },
+  },
+};
