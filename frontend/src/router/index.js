fc257460ef75d4d46253fa1e7e694bcb2d47267d
NevenDavidovic <nenoronnie@gmmail.com>
2024-02-17 13:41:05 +0100

    adding the organization route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import OrganizationView from "@/views/OrganizationView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/organizations",
+    name: "organizations",
+    component: OrganizationView,
   },
 ];
 
