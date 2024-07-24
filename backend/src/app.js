4db540a21975a7d16ab9bd2b7b8a524fcb4ca659
NevenDavidovic <nenoronnie@gmmail.com>
2024-07-24 22:13:49 +0200

    src

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,27 +21,6 @@ const PORT = process.env.PORT || 8081;
 const bcrypt = require("bcrypt");
 let loggedInUserId = null;
 
-const { Pool } = require("pg"); // Import Pool class from pg library
-
-const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "bilify",
-  password: process.env.DB_PASSWORD || "root",
-  port: process.env.DB_PORT || 5433,
-});
-
-function handleDbError(err) {
-  console.error("Error connecting to the PostgreSQL database:", err.stack);
-  process.exit(1); // Exit the application on database connection error (optional: handle gracefully)
-}
-
-pool.on("connect", () => {
-  console.log("Connected to the PostgreSQL database");
-});
-
-pool.on("error", handleDbError);
-
 app.post("/logout", (req, res) => {
   console.log("Request received on /logout route!");
   try {
