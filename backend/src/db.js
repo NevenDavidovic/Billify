e4a8157f129049d7fc7872c16d2d531092a99d78
NevenDavidovic <nenoronnie@gmmail.com>
2024-08-01 21:02:33 +0200

    db

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,5 +1,13 @@
 const { Pool } = require("pg"); // Import Pool class from pg library
 
+console.log({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
 const pool = new Pool({
   user: process.env.DB_USER || "postgres",
   host: process.env.DB_HOST || "localhost",
